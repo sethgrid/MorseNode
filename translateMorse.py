831d2a519dfbd6789ce6740b3d9f0327bbc83317
Seth Ammons <seth.ammons@sendgrid.com>
2012-12-09 20:55:50 -0800

    switched default starting string back to hello world

--- translateMorse.py
+++ translateMorse.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     starting_string = '......-...-..---.-----.-..-..-..' # hello world
     #starting_string = '......' # hi
     #starting_string = '-.-.-----.--...--..-.' # computer
-    starting_string = '-.....--.-..-..-.-.-.--....-.---.---...-.----..-.---..---.--....---...-..-.-......-...---..-.---..-----.'
+    #starting_string = '-.....--.-..-..-.-.-.--....-.---.---...-.----..-.---..---.--....---...-..-.-......-...---..-.---..-----.'
 
     Morse = DecodeMorse(starting_string)
     Morse.translate()
