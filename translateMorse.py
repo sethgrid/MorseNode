a6e25a6a8fac0700137778c3e4e7ac427aabc838
Seth Ammons <seth.ammons@sendgrid.com>
2012-12-09 20:15:44 -0800

    more info on results displayed

--- translateMorse.py
+++ translateMorse.py
@@ -4,7 +4,20 @@ if __name__ == '__main__':
     starting_string = '......-...-..---.-----.-..-..-..' # hello world
     #starting_string = '......' # hi
     #starting_string = '-.-.-----.--...--..-.' # computer
+    starting_string = '-.....--.-..-..-.-.-.--....-.---.---...-.----..-.---..---.--....---...-..-.-......-...---..-.---..-----.'
 
     Morse = DecodeMorse(starting_string)
     Morse.translate()
     Morse.showMatches()
+
+    match_list = Morse.getMatches()
+    longest = max(match_list, key=len)
+    shortest = min(match_list, key=len)
+    least_words = min(match_list, key=lambda x: len(x.split()))
+
+
+    print 'interesting data:'
+    print 'longest match: %s' % longest
+    print 'shortest match: %s' % shortest
+    print 'least words: %s' % least_words
+
